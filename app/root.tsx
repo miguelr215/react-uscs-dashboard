ce82c397af6eed3e02e6445fb55c38badeaf7e16
miguelr215 <drexelramos@gmail.com>
2025-10-11 07:55:13 -0400

    switch to bs icons

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
