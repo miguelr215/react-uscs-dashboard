f84db671a3277ec3c5202ea13be43e01b39f5eb9
miguelr215 <drexelramos@gmail.com>
2025-10-11 07:29:13 -0400

    init commit

---
+++ app/routes/404.tsx
@@ -0,0 +1,7 @@
+const DidNotFind = () => {
+    return (
+        <div>Sorry, we couldn't find that page.</div>
+    )
+}
+
+export default DidNotFind
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,6 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { FaBeer } from "react-icons/fa";
 
 export function Welcome() {
   return (
@@ -19,6 +20,9 @@ export function Welcome() {
             />
           </div>
         </header>
+        <div>
+          beer is good <FaBeer />
+        </div>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
