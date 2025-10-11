90f29a42ce77fc73ecdd223f352a910f3ef3cb03
miguelr215 <drexelramos@gmail.com>
2025-10-11 14:46:02 -0400

    sidebar

--- components/LogoLink.tsx
+++ components/LogoLink.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router'
 
 const LogoLink = () => {
     return (
-        <Link to="/" className="max-w-[125px]">
+        <Link to="/" className="max-w-[125px] md:max-w-[155px]">
             <img src="https://www.uscold.com/wp-content/uploads/2023/07/USCS_Logo_Horizontal_Email.png" alt="US Cold Storage logo" />
         </Link>
     )
