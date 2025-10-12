cbaaf877b44ba9274a06a7ab27a703e318f91438
miguelr215 <drexelramos@gmail.com>
2025-10-12 07:38:06 -0400

    sidebar footer

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router"
 import LogoLink from "./LogoLink";
+import SidebarFooter from "./SidebarFooter";
 
 const MobileSidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,23 +16,28 @@ const MobileSidebar = () => {
                     </button>
                 </header>
 
-                <div className={`mobile-sidebar-content fixed top-0 left-0 h-screen w-52 bg-gray-200 transform transition-transform duration-300 ${isOpen ? "translate-x-0 z-50" : "-translate-x-full"}`}>
-                    <nav className="flex flex-col p-4 gap-4">
-                        <Link to="/" className="db-nav-btn" onClick={() => setIsOpen(false)}>
-                            <i className="bi bi-house-door"></i> Home
-                        </Link>
-                        <Link to="/dashboard" className="db-nav-btn" onClick={() => setIsOpen(false)}>
-                            <i className="bi bi-bar-chart-fill"></i> Dashboard
-                        </Link>
-                        <Link to="/dashboard/inventory" className="db-nav-btn" onClick={() => setIsOpen(false)}>
-                            <i className="bi bi-boxes"></i>
-                            Inventory
-                        </Link>
-                        <Link to="/dashboard/sales" className="db-nav-btn" onClick={() => setIsOpen(false)}>
-                            <i className="bi bi-piggy-bank-fill"></i>
-                            Sales
-                        </Link>
-                    </nav>
+                <div className={`fixed top-0 left-0 h-screen w-52 bg-gray-200 transform transition-transform duration-300 ${isOpen ? "translate-x-0 z-50" : "-translate-x-full"}`}>
+                    <div className="flex flex-col h-full justify-between">
+                        <nav className="flex flex-col p-4 gap-4">
+                            <Link to="/" className="db-nav-btn" onClick={() => setIsOpen(false)}>
+                                <i className="bi bi-house-door"></i> Home
+                            </Link>
+                            <Link to="/dashboard" className="db-nav-btn" onClick={() => setIsOpen(false)}>
+                                <i className="bi bi-bar-chart-fill"></i> Dashboard
+                            </Link>
+                            <Link to="/dashboard/inventory" className="db-nav-btn" onClick={() => setIsOpen(false)}>
+                                <i className="bi bi-boxes"></i>
+                                Inventory
+                            </Link>
+                            <Link to="/dashboard/sales" className="db-nav-btn" onClick={() => setIsOpen(false)}>
+                                <i className="bi bi-piggy-bank-fill"></i>
+                                Sales
+                            </Link>
+                        </nav>
+
+                        <SidebarFooter />
+
+                    </div>
                 </div>
             </div>
             {isOpen && (
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router'
 import LogoLink from './LogoLink'
+import SidebarFooter from './SidebarFooter'
 
 const Sidebar = () => {
     return (
         <aside className='w-full max-w-[13rem] h-screen shadow-2xl shadow-gray-500 hidden md:block'>
-            <div className={`mobile-sidebar-content h-screen w-52 bg-gray-200 z-50`}>
+            <div className={`flex flex-col justify-between h-screen w-52 bg-gray-200 z-50`}>
                 <nav className="flex flex-col p-4 gap-4">
                     <LogoLink />
                     <Link to="/dashboard" className="db-nav-btn">
@@ -20,6 +21,8 @@ const Sidebar = () => {
                         Sales
                     </Link>
                 </nav>
+
+                <SidebarFooter />
             </div>
         </aside>
     )
