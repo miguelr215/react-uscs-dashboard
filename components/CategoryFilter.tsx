9291f1a76f532094bbe1b7ab5716a982647ae895
miguelr215 <drexelramos@gmail.com>
2025-10-13 06:42:28 -0400

    category filter

--- components/CategoryFilter.tsx
+++ components/CategoryFilter.tsx
@@ -1,8 +1,25 @@
+import { useData } from 'hooks/useData'
 import React from 'react'
 
 const CategoryFilter = () => {
+    const { categories, currentCategory, changeCurrentCategory } = useData();
+
     return (
-        <div>CategoryFilter</div>
+        <div className='mb-4 md:mb-5'>
+            <p className='text-xs text-gray-500 mb-1 md:mb-2'>Filter by category:</p>
+            <ul className='flex flex-wrap gap-4 md:gap-5'>
+                {categories.map((category) => (
+                    <li key={category}>
+                        <button
+                            onClick={() => changeCurrentCategory(category)}
+                            className={`db-cat-btn ${currentCategory === category ? 'font-bold' : ''}`}
+                        >
+                            {category}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
