7e5fbb3bf60a119bb3ae3d3f8b6dea33e9f46024
miguelr215 <drexelramos@gmail.com>
2025-10-13 21:06:26 -0400

    dashboard charts

--- app/routes/dashboard/dashboard.tsx
+++ app/routes/dashboard/dashboard.tsx
@@ -1,106 +1,78 @@
-import React, { useEffect } from 'react';
-import { Header } from 'components';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
-
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top' as const,
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart',
-        },
-    },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-// export const data = {
-//     labels,
-//     datasets: [
-//         {
-//             label: 'Dataset 1',
-//             data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//             borderColor: 'rgb(255, 99, 132)',
-//             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//         },
-//         {
-//             label: 'Dataset 2',
-//             data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//             borderColor: 'rgb(53, 162, 235)',
-//             backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//         },
-//     ],
-// };
-
-const data = [
-    { month: "January", amount: 50 },
-    { month: "February", amount: 44 },
-    { month: "March", amount: 32 },
-    { month: "April", amount: 46 },
-    { month: "May", amount: 33 },
-    { month: "June", amount: 24 },
-    { month: "July", amount: 29 },
-    { month: "August", amount: 36 },
-    { month: "September", amount: 40 },
-    { month: "October", amount: 42 },
-    { month: "November", amount: 38 },
-    { month: "December", amount: 45 },
-];
+import React, { useEffect, useState } from 'react';
+import { CategoryFilter, DoughnutChart, Header, LineChart, LoadingSpinner } from 'components';
+import { useData } from 'hooks/useData';
+import { useDoughnutChartFormat, type DoughnutChartDataType } from 'hooks/useDoughnutChartFormat';
+import { useCategoryFilter } from 'hooks/useCategoryFilter';
+import { useSalesAmt } from 'hooks/useSalesAmt';
+import { useSalesRevenue } from 'hooks/useSalesRevenue';
+import { useSumsOfArrays } from 'hooks/useSumsOfArrays';
+import { useLineChartFormat, type LineChartDataType } from 'hooks/useLineChartFormat';
+import { useDoughnutData } from 'hooks/useDoughnutData';
+import { useDoughnutCatLabels } from 'hooks/useDoughnutCatLabels';
 
 const Dashboard = () => {
+    const [lineAmtChartData, setLineAmtChartData] = useState<LineChartDataType | null>(null);
+    const [lineRevChartData, setLineRevChartData] = useState<LineChartDataType | null>(null);
+    const [doughnutChartData, setDoughnutChartData] = useState<DoughnutChartDataType | null>(null);
+    const { products, categories, currentCategory, loading, error } = useData();
+
+    useEffect(() => {
+        // line chart
+        const filteredProducts = useCategoryFilter(products, currentCategory);
+        const salesAmtArrNumsArr = useSalesAmt(filteredProducts);
+        const salesRevArrNumsArr = useSalesRevenue(filteredProducts);
+        const salesAmtArrTotals = useSumsOfArrays(salesAmtArrNumsArr);
+        const salesRevArrTotals = useSumsOfArrays(salesRevArrNumsArr);
+        const salesAmtChartDataFormatted = useLineChartFormat(`12 Month Sales Amount for ${currentCategory}`, salesAmtArrTotals);
+        const salesRevChartDataFormatted = useLineChartFormat(`12 Month Sales Revenue $ for ${currentCategory}`, salesRevArrTotals);
+
+        // doughnut chart
+        const doughnutChartDataArr = useDoughnutData(filteredProducts, currentCategory);
+        const doughnutLabels = currentCategory === 'All' ? categories.slice(1) : useDoughnutCatLabels(filteredProducts, currentCategory)
+        const doughnutChartDataFormatted = useDoughnutChartFormat(`Current Inventory for ${currentCategory}`, doughnutChartDataArr, doughnutLabels);
+
+        setLineAmtChartData(salesAmtChartDataFormatted);
+        setLineRevChartData(salesRevChartDataFormatted);
+        setDoughnutChartData(doughnutChartDataFormatted);
+    }, [products, currentCategory]);
+
+    if (loading) return <LoadingSpinner />
+    if (error) return <p className='text-red-500'>Error: {error}</p>
 
     return (
         <section>
             <Header title="Dashboard" description="View popular Dashboards" />
-            <div className="dashboard-content">
-                current inventory by category - doughnut
-                current sales by category - doughnut
-                total inventory last 12 months - line
-                total sales last 12 months - line
-                total revenue last 12 months - line
-                <div className="sm-chart">
-                    <Line
-                        data={{
-                            labels: labels,
-                            datasets: [
-                                {
-                                    label: 'set1',
-                                    data: [5, 6, 7, 12, 15, 10, 7, 9, 16, 21, 18, 15],
-                                    borderColor: 'rgb(255, 99, 132)',
-                                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                                },
-                                {
-                                    label: 'set2',
-                                    data: [3, 2, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-                                    borderColor: 'rgb(53, 162, 235)',
-                                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
-                                },
-                            ],
-                        }}
-                    />
+            <CategoryFilter />
+            <div className='flex flex-col gap-6 items-center lg:flex-row'>
+                {/* Doughnut chart */}
+                {doughnutChartData ? (
+                    <div className='w-full max-w-xl lg:max-w-lg'>
+                        <p className="text-center text-xs text-gray-700 lg:text-left lg:pl-10">Current Inventory:</p>
+                        <DoughnutChart data={doughnutChartData} />
+                    </div>
+                ) : (
+                    <LoadingSpinner />
+                )}
+
+                <div className="flex flex-col gap-6">
+                    {/* Sales Amt last 12 months */}
+                    {lineAmtChartData ? (
+                        <div className="w-full max-w-xl">
+                            <LineChart data={lineAmtChartData} />
+                        </div>
+                    ) : (
+                        <LoadingSpinner />
+                    )}
+
+                    {/* Sales Rev last 12 months */}
+                    {lineRevChartData ? (
+                        <div className="w-full max-w-xl">
+                            <LineChart data={lineRevChartData} />
+                        </div>
+                    ) : (
+                        <LoadingSpinner />
+                    )}
+
                 </div>
 
             </div>
