fda052e8558fde46cb87039283117218780211f0
miguelr215 <drexelramos@gmail.com>
2025-10-27 08:34:21 -0400

    gh-pages

--- hooks/useData.ts
+++
@@ -1,8 +0,0 @@
-import { DashboardContext } from "context/DashboardContext";
-import { useContext } from "react";
-
-export function useData() {
-	const context = useContext(DashboardContext);
-	if (!context) throw new Error("useData must be used within a DataProvider");
-	return context;
-}
